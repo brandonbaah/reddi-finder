dc91f75fd3bd048703cc71d3dc5eabfea6387c48
brandonbaah <brandonbaah@gmail.com>
2019-06-06 15:58:25 -0400

    'started_project'

---
+++ index.js
@@ -0,0 +1,12 @@
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+
+searchForm.addEventListener('submit', e => {
+    // Get the search term
+    const searchTerm = searchInput.value;
+    // Get sort
+    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+    console.log(sortBy);
+    
+    e.preventDefault();
+});
